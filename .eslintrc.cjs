6fdef739adeaae97752aca690f89eb52b5740e9e
Stevan Freeborn <65925598+StevanFreeborn@users.noreply.github.com>
2023-04-28 21:45:04 -0500

    chore: setup prettier config

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -4,14 +4,16 @@ module.exports = {
   extends: [
     'plugin:@typescript-eslint/recommended',
     'plugin:react-hooks/recommended',
+    'plugin:prettier/recommended',
   ],
   parser: '@typescript-eslint/parser',
   parserOptions: {
     ecmaVersion: 'latest',
     sourceType: 'module',
   },
-  plugins: ['react-refresh', 'prettier'],
+  plugins: ['react-refresh'],
   rules: {
     'react-refresh/only-export-components': 'warn',
+    'prettier/prettier': 'error',
   },
 };
