93d18a24489bf42dba58e9112325b224324b013c
Stevan Freeborn <65925598+StevanFreeborn@users.noreply.github.com>
2023-04-28 21:57:43 -0500

    chore: setup vitest

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,18 @@
+/// <reference types="vitest" />
 import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
   plugins: [react()],
   base: '/ListValueImageGenerator/',
+  test: {
+    environment: 'jsdom',
+    setupFiles: [],
+    testNamePattern: './tests/**/*.spec.tsx',
+    globals: true,
+    coverage: {
+      provider: 'istanbul',
+      reporter: ['html-spa', 'text', 'text-summary'],
+    },
+  },
 });
